f85292c18b27798a3603c0d6ace45f728b13ff7f
João Chaves <jomp@pop-os.localdomain>
2022-06-07 19:32:01 -0300

    implementando indicador de página atual no header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Header.css'
 import logo from '../../images/logo.png'
-import { Link } from 'react-router-dom'
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0, 
-      behavior: 'auto'
-    });
-  }
+import { Link, useLocation } from 'react-router-dom'
 
   export function Header() {
-  return (
+    const scrollToTop = () => {
+      window.scrollTo({
+        top: 0, 
+        behavior: 'auto'
+      });
+    }
+
+    const [status, setStatus] = useState('home')  
+    const url = useLocation().pathname;
+    
+    const changeRoute = () => {
+      scrollToTop()
+    }
+
+    useEffect(() => {
+      if(url == '/servicos') {
+        setStatus('servicos') 
+      }
+      else if(url == '/cases') {
+        setStatus('cases')
+      }
+      else {
+        setStatus('home')
+      }
+    },[url])
+
+    return (
     <div className='header-box'>
       <header>
         <figure>
@@ -26,11 +45,11 @@ import { Link } from 'react-router-dom'
             <span></span>
           </label>
           <ul>
-            <li onClick={scrollToTop}><Link to="/">Página Inicial</Link></li>
-            <li onClick={scrollToTop}><Link to="/servicos">Nossos Serviços</Link></li>
-            <li onClick={scrollToTop}><Link to="/cases">Cases</Link></li>
-            <li onClick={scrollToTop}><Link to="/">Seja um Parceiro</Link></li>
-            <li onClick={scrollToTop}><Link to="/">Contato</Link></li>
+            <li className={status == 'home' ? 'active' : ''} onClick={changeRoute}><Link to="/">Página Inicial</Link></li>
+            <li className={status == 'servicos' ? 'active' : ''} onClick={changeRoute}><Link to="/servicos">Nossos Serviços</Link></li>
+            <li className={status == 'cases' ? 'active' : ''} onClick={changeRoute}><Link to="/cases">Cases</Link></li>
+            <li onClick={changeRoute}><Link to="/">Seja um Parceiro</Link></li>
+            <li onClick={changeRoute}><Link to="/">Contato</Link></li>
           </ul>
         </nav>
       </header>
